79b7bb83c0dcaa615ab0939f2d760fc75862beed
gear <nnote09@gmail.com>
2019-10-08 11:56:20 +0900

    test

---
+++ src/components/HomeMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="logo">
+          <router-link class="logo" to="/">죽방</router-link>
+          <v-icon>mdi-anchor</v-icon>
+          <button @click="choi">test</button>
+
+          </div>
+          <div class="menu">
+            <ul>
+              <li><router-link to="/apart">아파트</router-link></li>
+              <li><router-link to="/village">빌라+투룸</router-link></li>
+              <li><router-link to="/oneRoom">원룸</router-link></li>
+              <li><router-link to="/office">오피스텔</router-link></li>
+            </ul>
+          </div>
+          <div class="juk-btn_login">
+            <router-link to="/login" class="button">로그인 / 회원가입</router-link>
+          </div>
+        <div class="mobile-menu"><i class="fa fa-bars"></i></div>
+      </div>
+  </header>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  methods: {
+    choi() {
+      axios({
+        url: 'http://localhost:8081',
+      })
+      .then(res => {
+        console.log(res);
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,48 @@
 <template>
-  <HelloWorld />
+  <div>
+    <HomeMenu />
+
+    <div class="wrapper">
+      <section class="statistics">
+        <div class="container">
+          <div class="statistics--item">
+            <img src="@/assets/img/jukdongsuk.png" alt="">
+          </div>
+          <div class="statistics--search">
+            <input type="text">
+            <button>찾아보기</button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <v-container>
+          <v-row>
+            <v-col>
+              <div class="title">계약요령</div>
+              <div class="content"></div>
+            </v-col>
+            <v-col>
+              <div class="title">뉴스</div>
+              <div class="content"></div>
+            </v-col>
+            <v-col>
+              <div class="title">공지사항</div>
+              <div class="content"></div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+import HomeMenu from '@/components/HomeMenu.vue';
 
 export default {
   components: {
-    HelloWorld,
+    HomeMenu,
   },
 };
 </script>
