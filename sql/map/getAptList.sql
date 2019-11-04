2e69c1a424a443895db0060d0711f9517c7073f2
gear <nnote09@gmail.com>
2019-11-04 15:50:31 +0900

    hotfix
    필터 적용

--- sql/map/getAptList.sql
+++ sql/map/getAptList.sql
@@ -21,7 +21,7 @@
 --     order by a.apt_seq) b
 -- where a.apt_seq = b.apt_seq
 
-select a.*, b.maxprice, b.minprice
+select a.*, b.maxprice, b.minprice, b.apt_area, b.apt_price, b.apt_deposit, b.build_type
 from
 		(
 		select  *
@@ -30,10 +30,16 @@ from
 		and     DISTNACE_WGS84(:lat, :lng, APT_LAT, APT_LNG) < 3.0
 		) a,
 		(
-		select max(apt_price) maxprice, min(apt_price) minprice, apt_seq
+		select  max(apt_price) maxprice,
+                min(apt_price) minprice,
+                round(avg(apt_area)) apt_area,
+                round(avg(apt_price)) apt_price,
+                round(avg(apt_deposit)) apt_deposit,
+                build_type,
+                apt_seq
 		from aptpurchase
 		where  build_type = nvl(:build_type, '매매')
 		and (sysdate - apt_contractdate) <= 180
-		group by apt_seq
+		group by apt_seq, build_type
 		) b
 where	a.apt_seq = b.apt_seq
