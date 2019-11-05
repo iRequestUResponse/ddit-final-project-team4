c26c8b74e8193b622940357383d73b0f34f9cb32
kang <khsok212@gmail.com>
2019-11-05 11:06:06 +0900

    오류 수정 및 디자인 수정

--- sql/apt/getSalesPrice.sql
+++ sql/apt/getSalesPrice.sql
@@ -1,4 +1,4 @@
-select CEIL(nvl(avg(price), 0))/10000 cnt from aptsales
+select round(nvl(avg(price), 0),-4)/10000 cnt from aptsales
 where apt_seq = :apt_seq
 and sales_type = '매매'
 and (sysdate - reg_date) <= 30
